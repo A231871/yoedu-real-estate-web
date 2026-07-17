b6e5a5d40cc31222a3cce50e34b45ef8434db8ac
FSXplay <skinoftheduck@gmail.com>
2026-07-17 14:25:00 +0800

    listing data module

---
+++ src/data/listings.ts
@@ -0,0 +1,21 @@
+import { listingControllerApi } from "../api/client";
+import {
+  ListingSummaryResponseListingTypeEnum,
+  type ListingDetailResponse,
+  type ListingSummaryResponse
+} from "../api/openapi-generated";
+
+export async function getListingSummaries(
+  listingType: ListingSummaryResponseListingTypeEnum,
+  page?: number,
+  size?: number,
+  sort?: Array<string>
+): Promise<ListingSummaryResponse[]> {
+  const { data } = await listingControllerApi.getListings({ listingType, page, size, sort });
+  return data.data?.content?.filter((listing) => listing.listingType === listingType) ?? [];
+}
+
+export async function getListingDetail(listingId: string): Promise<ListingDetailResponse | undefined> {
+  const { data } = await listingControllerApi.getListingDetail({ id: listingId });
+  return data.data;
+}
